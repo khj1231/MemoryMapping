c99f6f82ddf9c99ca549d19d121fba19d58f0828
khj1231 <51897119+khj1231@users.noreply.github.com>
2019-10-28 00:41:42 +0800

    初始化
    添加内存映射

---
+++ Test1027/Test1027/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Test1027
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(346, 11);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(769, 769);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column3,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(11, 67);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(331, 713);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(155, 22);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 19);
+            this.label2.TabIndex = 3;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Address";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "B";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "KB";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1125, 812);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+    }
+}
+
---
+++ Test1027/Test1027/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test1027
+{
+    public partial class Form1 : Form
+    {
+        public List<MyLocation> dictionary = new List<MyLocation>();
+        Graphics graphics;
+        SolidBrush myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Red);//画刷
+        Pen pen = new Pen(Color.Red, 3);
+        public Form1()
+        {
+            InitializeComponent();
+            dictionary.Add(new MyLocation(0, 0));
+            dictionary.Add(new MyLocation(255, 255));
+            graphics = this.pictureBox1.CreateGraphics();
+            refresh();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            BeginInvoke(new Action(() =>
+            {
+                label2.Text = "0x"  + Convert.ToString(e.Location.Y / 3, 16).PadLeft(2, '0') + "" + Convert.ToString(e.Location.X / 3, 16).PadLeft(2, '0') ;
+            }));
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                dictionary.Add(new MyLocation(e.Location.X / 3, e.Location.Y / 3));
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                dictionary.Remove(dictionary.Find(newlocation => e.Location.X / 3 == newlocation.Location_x && e.Location.Y / 3 == newlocation.Location_y));
+            }
+            refresh();
+
+        }
+
+        void refresh()
+        {
+            sort();
+            pictureBox1.Refresh();
+            dataGridView1.Rows.Clear();
+            for (int i = 0; i < dictionary.Count(); i++)
+            {
+                if (i > 0)
+                    graphics.FillRectangle(myBrush, new Rectangle(dictionary[i].Location_x *3, dictionary[i].Location_y * 3, 3, 3));
+                dataGridView1.Rows.Add("0x" +  Convert.ToString(dictionary[i].Location_y, 16).PadLeft(2, '0') +
+                    "" + Convert.ToString(dictionary[i].Location_x, 16).PadLeft(2, '0'), dictionary[i].Location_len, (float)dictionary[i].Location_len/1024);
+               // graphics.DrawLine(pen, dictionary[i - 1].Location_x * 3, dictionary[i - 1].Location_y * 3, dictionary[i].Location_x * 3, dictionary[i].Location_y * 3);
+            }
+            //dataGridView1.DataSource;
+        }
+
+        void sort()
+        {
+            dictionary = (from index in dictionary
+                          orderby index.Location_y * 256 * 3 + index.Location_x ascending
+                          select index).ToList();
+            for(int i = 1; i < dictionary.Count; ++i)
+            {
+                    dictionary[i].Location_len = ((dictionary[i].Location_y - dictionary[i - 1].Location_y) * 256 + dictionary[i].Location_x - dictionary[i - 1].Location_x) * 4;
+            }
+            dictionary[dictionary.Count - 1].Location_len += 4;
+        }
+
+        public class MyLocation
+        {
+            private int location_x;
+            private int location_y;
+            private int location_len = 0;
+            public int Location_x { get => location_x; set => location_x = value; }
+            public int Location_y { get => location_y; set => location_y = value; }
+            public int Location_len { get => location_len; set => location_len = value; }
+
+            public MyLocation(int lox, int loy)
+            {
+                Location_x = lox;
+                Location_y = loy;
+            }
+        }
+
+        private void dataGridView1_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+                if (dataGridView1.Rows.Count > 0 && e.RowIndex > 0 && e.RowIndex < dataGridView1.Rows.Count -2)
+                {
+                    dictionary.RemoveAt(e.RowIndex);
+                    //dictionary.Remove(dictionary.Find(newlocation => Convert.ToInt32(dataGridView1[0,e.RowIndex].Value) == newlocation.Location_x && Convert.ToInt32(dataGridView1[1, e.RowIndex].Value) == newlocation.Location_y));
+                    refresh();
+                }
+        }
+    }
+}
